cab4b376f75605f72b8591a0f05af312bffd7f15
claraherscu <clara.herscu@mail.huji.ac.il>
2017-01-04 14:53:02 +0200

    commit correcting get24Hour

--- eventPage/urls.py
+++ eventPage/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'getTodaysEvents/$', views.get_next_events_list, name='events_list'),
     url(r'^$', views.index, name='index'),
 ]
--- eventPage/views.py
+++ eventPage/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from eventPage import models
+from django.core import serializers
+
 
 def index(request):
-    return HttpResponse("response for example")
+    # return HttpResponse("response for example")
+    html = "<html><body><a href='127.0.0.1:8000/eventPage/getTodaysEvents?ID=302184452'>get " \
+           "events</a></body></html>"
+    return HttpResponse(html)
+
+
+def get_next_events_list(request):
+    ID = request.GET.values()[0]
+    events = models.get24HourEvents(ID)
+    events_json = serializers.serialize("json", events)
+    return JsonResponse(events_json)
